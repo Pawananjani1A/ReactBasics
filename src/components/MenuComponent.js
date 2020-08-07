55a8f3fddd25f22dcef45e783278a6d015513d0b
Pawananjani1A <pawananjanimth1@gmail.com>
2020-08-07 16:11:10 +0530

    Component LifeCyvle Methods

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -9,6 +9,14 @@ class Menu extends Component{
     this.state={
       selectedDish:null
     };
+
+    console.log("Menu Component constructor is invoked");
+  }
+
+  componentDidMount()
+  {
+    console.log("Menu Component componentDidMount is invoked");
+
   }
 
   onDishSelect(dish)
@@ -56,6 +64,8 @@ class Menu extends Component{
         </div>
       )
     });
+
+    console.log("Menu Component render is invoked");
 
     return (
       <div className="container">
